2249fb017b84b22b65e890f4fa5f8ddbbb9e50e8
Ludovic <ludovic@jeedom.com>
2016-12-04 19:15:29 +0100

    changed font

--- desktop/php/broadlink.php
+++ desktop/php/broadlink.php
@@ -96,7 +96,7 @@ foreach ($eqLogics as $eqLogic) {
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-  <a class="btn btn-success eqLogicAction pull-right learnCommand"><i class="fa fa-check-circle"></i> {{Apprendre une commande}}</a>
+  <a class="btn btn-success eqLogicAction pull-right learnCommand"><i class="fa fa-rss"></i> {{Apprendre une commande}}</a>
 
   <ul class="nav nav-tabs" role="tablist">
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
