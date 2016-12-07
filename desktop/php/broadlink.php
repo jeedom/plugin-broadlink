c4bdd5b5ba21ce2765bb108bf89164c9e5bb7fe1
Ludovic <ludovic@jeedom.com>
2016-12-07 11:55:47 +0100

    update

--- desktop/php/broadlink.php
+++ desktop/php/broadlink.php
@@ -96,9 +96,11 @@ foreach ($eqLogics as $eqLogic) {
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+ <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
   <a class="btn btn-success eqLogicAction pull-right learnCommand"><i class="fa fa-rss"></i> {{Apprendre une commande}}</a>
 
   <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
   </ul>
@@ -109,10 +111,6 @@ foreach ($eqLogics as $eqLogic) {
         <div class="col-sm-6">
           <form class="form-horizontal">
             <fieldset>
-              <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}
-                <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-                <a class="btn btn-xs btn-default pull-right eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
-              </legend>
               <div class="form-group">
                 <label class="col-sm-3 control-label">{{Nom de l'équipement broadlink}}</label>
                 <div class="col-sm-4">
@@ -181,12 +179,6 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
             <label class="col-sm-3 control-label">{{Délai maximum autorisé entre 2 messages (min)}}</label>
             <div class="col-sm-4">
               <input class="eqLogicAttr form-control" data-l1key="timeout" />
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="col-sm-3 control-label">{{Commentaire}}</label>
-            <div class="col-sm-8">
-              <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
             </div>
           </div>
         </fieldset>
