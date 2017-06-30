eb6f5ee49f6e6efa27429373070dbe5e4ac778db
Loïc <loic@jeedom.com>
2017-06-30 12:12:03 +0200

    Update and rename install.sh to install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,14 +1,18 @@
-touch /tmp/dependancy_broadlink_in_progress
-echo 0 > /tmp/dependancy_broadlink_in_progress
+PROGRESS_FILE=/tmp/dependancy_broadlink_in_progress
+if [ ! -z $1 ]; then
+	PROGRESS_FILE=$1
+fi
+touch ${PROGRESS_FILE}
+echo 0 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation des dépendances             *"
 echo "********************************************************"
 apt-get update
-echo 50 > /tmp/dependancy_broadlink_in_progress
+echo 50 > ${PROGRESS_FILE}
 sudo pip install pycrypto
 sudo pip install pyudev
-echo 100 > /tmp/dependancy_broadlink_in_progress
+echo 100 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation terminée                    *"
 echo "********************************************************"
-rm /tmp/dependancy_broadlink_in_progress
+rm ${PROGRESS_FILE}
