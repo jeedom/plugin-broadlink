e6eadb3b5600465924e94f5ed2ed9f4b572529b5
sarakha63 <sarakha_ludovic@yahoo.fr>
2019-09-30 17:40:19 +0200

    ui

--- desktop/php/broadlink.php
+++ desktop/php/broadlink.php
@@ -76,7 +76,7 @@ foreach ($eqLogics as $eqLogic) {
 <div class="col-lg-12 eqLogic" style="display: none;">
 	<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
-				<a class="btn btn-success eqLogicAction learnCommand roundedLeft"><i class="fa fa-rss"></i> {{Apprendre une commande}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+				<a class="btn btn-success btn-sm eqLogicAction learnCommand roundedLeft"><i class="fa fa-rss"></i> {{Apprendre une commande}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
 		</div>
   <ul class="nav nav-tabs" role="tablist">
